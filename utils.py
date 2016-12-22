bca8c963df08e6cb8b81786b4649e20170c33607
strobostro <strobostro@gmail.com>
2016-12-22 12:23:09 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,36 @@
+import geoip2.database, _mysql, ConfigParser
+
+conf = ConfigParser.ConfigParser()
+conf.read('./config.cfg')
+
+geoip = conf.get("geodb", "GeoDB")
+geodb = geoip2.database.Reader(geoip)
+
+db=_mysql.connect(host=conf.get("honeydb", "DbHost"),user=conf.get("honeydb", "DbUser"),passwd=conf.get("honeydb", "DbPasswd"),db=conf.get("honeydb", "DbName"))
+
+def get_country(ip):
+        country=""
+        try:
+                country = geodb.country(ip).country.iso_code
+        except geoip2.errors.AddressNotFoundError:
+                country = "Unknown"
+        return country
+
+def insert_into_db(srvport, saddr, scountry, sport, data):
+        query = "insert into connections (time, server_port, source_address, source_country, source_port, data) values (now(), "+srvport+", '"+saddr+"', '"+scountry+"', "+sport+", '"+str(data)+"')"
+        db.query(query)
+        db.store_result()
+
+def get_static_ports():
+        static_ports = set()
+        ports = conf.get("honeyconf","StaticPorts")
+        for port in ports.split(","):
+                static_ports.add(int(port))
+        print static_ports
+        return static_ports
+
+def get_log_others():
+        return int(conf.get("honeyconf","LogOthers"))
+
+def get_srv_ip():
+        return conf.get("honeyconf","ServerIP")
